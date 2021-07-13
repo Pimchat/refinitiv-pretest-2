a887bed1dd1554aae9987d1f256c69620bb8f445
PIMCHAT PANICHSUKKO <pim.pimchat@gmail.com>
2021-07-13 17:52:26 +0700

    add appjscss

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [searchItem, setsearchItem] = useState("");
+
+  useEffect(() => {
+    fetch("https://api.publicapis.org/categories")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setItems(data);
+      });
+  }, []);
+
+  const listItems = items
+    .filter((item) => {
+      if (searchItem === "") {
+        return item;
+      } else if (item.toLowerCase().includes(searchItem.toLowerCase())) {
+        return item;
+      }
+    })
+    .map((val, key) => (
+      <div className="cell" key={key}>
+        {val}
+      </div>
+    ));
+
+  const handleSearch = (e) => {
+    console.log(e.target.value);
+    setsearchItem(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="box">
+      <input
+        className="input-box"
+        type="text"
+        value={searchItem}
+        placeholder="Search..."
+        onChange={handleSearch}
+      />
+
+      <div className="table">{listItems}</div>
     </div>
   );
 }
